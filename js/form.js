ebf959cca1266ba1353280b56bd8431140690b3b
KoshcheevPA <pavelkoshcheev@yandex.ru>
2018-12-07 22:14:33 +0500

    Создан модуль формы

---
+++ js/form.js
@@ -0,0 +1,68 @@
+(function () {
+
+  var priceInput = adForm.querySelector('#price');
+  var typeSelect = adForm.querySelector('#type');
+  var timeInSelect = adForm.querySelector('#timein');
+  var timeOutSelect = adForm.querySelector('#timeout');
+  var roomNumberSelect = adForm.querySelector('#room_number');
+  var guestRoomSelect = adForm.querySelector('#capacity');
+
+  var guestsAllOptions = guestRoomSelect.querySelectorAll('option');
+
+
+  priceInput.placeholder = MIN_FLAT_PRICE;
+  typeSelect.addEventListener('change', function () {
+    if (typeSelect.value === 'bungalo') {
+      priceInput.min = MIN_BUNGALO_PRICE;
+    } else if (typeSelect.value === 'flat') {
+      priceInput.min = MIN_FLAT_PRICE;
+    } else if (typeSelect.value === 'house') {
+      priceInput.min = MIN_HOUSE_PRICE;
+    } else if (typeSelect.value === 'palace') {
+      priceInput.min = MIN_PALACE_PRICE;
+    }
+    priceInput.placeholder = priceInput.min;
+  });
+
+  timeInSelect.addEventListener('change', function () {
+    timeOutSelect.selectedIndex = timeInSelect.selectedIndex;
+  });
+
+  timeOutSelect.addEventListener('change', function () {
+    timeInSelect.selectedIndex = timeOutSelect.selectedIndex;
+  });
+
+  var setDisabled = function (element) {
+    element.setAttribute('disabled', true);
+  };
+
+  var removeDisabled = function (element) {
+    element.removeAttribute('disabled');
+  };
+
+  var guestsNumbersObject = {
+    1: [1],
+    2: [1, 2],
+    3: [1, 2, 3],
+    100: [0]
+  };
+
+  var getGuestOptions = function (array) {
+    for (var j = 0; j < guestsAllOptions.length; j++) {
+      setDisabled(guestsAllOptions[j]);
+    }
+    for (var i = 0; i < array.length; i++) {
+      var guestOptions = guestRoomSelect.querySelector('[value="' + array[i] + '"]');
+      removeDisabled(guestOptions);
+    }
+  };
+
+  roomNumberSelect.addEventListener('change', function () {
+    if (roomNumberSelect.value === '100') {
+      guestRoomSelect.value = '0';
+    } else {
+      guestRoomSelect.value = roomNumberSelect.value;
+    }
+    getGuestOptions(guestsNumbersObject[roomNumberSelect.value]);
+  });
+})();
--- js/map.js
+++ js/map.js
@@ -115,6 +115,8 @@ var getOffers = function (offersNumber) {
   return offersArray;
 };
 
+//СОЗДАНИЕ МЕТОК
+
 var similarPinElement = document.querySelector('.map__pin');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinFragment = document.createDocumentFragment();
@@ -130,6 +132,8 @@ var getNewPin = function (array) {
   }
   mapPins.appendChild(pinFragment);
 };
+
+//СОЗДАНИЕ КАРТОЧЕК
 
 var similarCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var renderCard = function (object) {
@@ -179,6 +183,8 @@ var addCard = function () {
   map.insertBefore(cardFragment, mapFilters);
 };
 
+
+// АКТИВАЦИЯ ФОРМЫ
 
 var onFormActivate = function () {
   map.classList.remove('map--faded');
@@ -242,74 +248,76 @@ var onMainPinActivateEnter = function (evt) {
   }
 };
 
-// Условия заполнения полей формы module4-task2
+// ЗАПОЛНЕНИЕ ФОРМЫ
+//
+// var priceInput = adForm.querySelector('#price');
+// var typeSelect = adForm.querySelector('#type');
+// var timeInSelect = adForm.querySelector('#timein');
+// var timeOutSelect = adForm.querySelector('#timeout');
+// var roomNumberSelect = adForm.querySelector('#room_number');
+// var guestRoomSelect = adForm.querySelector('#capacity');
+//
+// var guestsAllOptions = guestRoomSelect.querySelectorAll('option');
+//
+//
+// priceInput.placeholder = MIN_FLAT_PRICE;
+// typeSelect.addEventListener('change', function () {
+//   if (typeSelect.value === 'bungalo') {
+//     priceInput.min = MIN_BUNGALO_PRICE;
+//   } else if (typeSelect.value === 'flat') {
+//     priceInput.min = MIN_FLAT_PRICE;
+//   } else if (typeSelect.value === 'house') {
+//     priceInput.min = MIN_HOUSE_PRICE;
+//   } else if (typeSelect.value === 'palace') {
+//     priceInput.min = MIN_PALACE_PRICE;
+//   }
+//   priceInput.placeholder = priceInput.min;
+// });
+//
+// timeInSelect.addEventListener('change', function () {
+//   timeOutSelect.selectedIndex = timeInSelect.selectedIndex;
+// });
+//
+// timeOutSelect.addEventListener('change', function () {
+//   timeInSelect.selectedIndex = timeOutSelect.selectedIndex;
+// });
+//
+// var setDisabled = function (element) {
+//   element.setAttribute('disabled', true);
+// };
+//
+// var removeDisabled = function (element) {
+//   element.removeAttribute('disabled');
+// };
+//
+//
+// var guestsNumbersObject = {
+//   1: [1],
+//   2: [1, 2],
+//   3: [1, 2, 3],
+//   100: [0]
+// };
+//
+// var getGuestOptions = function (array) {
+//   for (var j = 0; j < guestsAllOptions.length; j++) {
+//     setDisabled(guestsAllOptions[j]);
+//   }
+//   for (var i = 0; i < array.length; i++) {
+//     var guestOptions = guestRoomSelect.querySelector('[value="' + array[i] + '"]');
+//     removeDisabled(guestOptions);
+//   }
+// };
+//
+// roomNumberSelect.addEventListener('change', function () {
+//   if (roomNumberSelect.value === '100') {
+//     guestRoomSelect.value = '0';
+//   } else {
+//     guestRoomSelect.value = roomNumberSelect.value;
+//   }
+//   getGuestOptions(guestsNumbersObject[roomNumberSelect.value]);
+// });
 
-var priceInput = adForm.querySelector('#price');
-var typeSelect = adForm.querySelector('#type');
-var timeInSelect = adForm.querySelector('#timein');
-var timeOutSelect = adForm.querySelector('#timeout');
-var roomNumberSelect = adForm.querySelector('#room_number');
-var guestRoomSelect = adForm.querySelector('#capacity');
-
-var guestsAllOptions = guestRoomSelect.querySelectorAll('option');
-
-
-priceInput.placeholder = MIN_FLAT_PRICE;
-typeSelect.addEventListener('change', function () {
-  if (typeSelect.value === 'bungalo') {
-    priceInput.min = MIN_BUNGALO_PRICE;
-  } else if (typeSelect.value === 'flat') {
-    priceInput.min = MIN_FLAT_PRICE;
-  } else if (typeSelect.value === 'house') {
-    priceInput.min = MIN_HOUSE_PRICE;
-  } else if (typeSelect.value === 'palace') {
-    priceInput.min = MIN_PALACE_PRICE;
-  }
-  priceInput.placeholder = priceInput.min;
-});
-
-timeInSelect.addEventListener('change', function () {
-  timeOutSelect.selectedIndex = timeInSelect.selectedIndex;
-});
-
-timeOutSelect.addEventListener('change', function () {
-  timeInSelect.selectedIndex = timeOutSelect.selectedIndex;
-});
-
-var setDisabled = function (element) {
-  element.setAttribute('disabled', true);
-};
-
-var removeDisabled = function (element) {
-  element.removeAttribute('disabled');
-};
-
-
-var guestsNumbersObject = {
-  1: [1],
-  2: [1, 2],
-  3: [1, 2, 3],
-  100: [0]
-};
-
-var getGuestOptions = function (array) {
-  for (var j = 0; j < guestsAllOptions.length; j++) {
-    setDisabled(guestsAllOptions[j]);
-  }
-  for (var i = 0; i < array.length; i++) {
-    var guestOptions = guestRoomSelect.querySelector('[value="' + array[i] + '"]');
-    removeDisabled(guestOptions);
-  }
-};
-
-roomNumberSelect.addEventListener('change', function () {
-  if (roomNumberSelect.value === '100') {
-    guestRoomSelect.value = '0';
-  } else {
-    guestRoomSelect.value = roomNumberSelect.value;
-  }
-  getGuestOptions(guestsNumbersObject[roomNumberSelect.value]);
-});
+//ПИН ГЛАВНОЙ КАРТЫ СОБЫТИЕ
 
 mainPin.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
